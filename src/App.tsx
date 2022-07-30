960081907321cc9e18f227b8a6dd56ee288ebf95
Karn-P <karnapper@gmail.com>
2022-07-30 16:55:00 +0700

    Initial github repos lists project.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="header-bar">
+        <h1>Listing Github Repos APP</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
