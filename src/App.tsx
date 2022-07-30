eb07b8ceaffa4db504146973382440575b4c0936
Karn-P <karnapper@gmail.com>
2022-07-30 16:59:01 +0700

    update: Add fetching function using useEffect and axios.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [url, setUrl] = useState<string>("https://api.github.com/repositories");
+  const [repos, setRepos] = useState<any[]>([]);
+
+  const fetchRepos = async () => {
+    await axios
+      .get(url)
+      .then((res) => {
+        setRepos(res.data);
+        console.log(res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchRepos();
+  }, []);
+
   return (
     <div className="container">
       <div className="header-bar">
